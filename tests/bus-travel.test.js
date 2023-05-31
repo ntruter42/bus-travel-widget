b8bec13f2765724e752002de2b588be74a5f33a1
ntruter42 <ntruter42@gmail.com>
2023-05-31 09:59:20 +0200

    Add index.html, css/, js/, tests/ - created files needed for widget and setup basic mocha test structure template

---
+++ tests/bus-travel.test.js
@@ -0,0 +1,12 @@
+describe('BusTravel', function () {
+	beforeEach(function () {
+		let busTravel = BusTravel();
+	})
+
+	// template to copy & paste
+	describe('FunctionName', function () {
+		it('test description', function () {
+			assert.equal(true, true);
+		});
+	});
+});
